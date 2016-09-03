f5794b7d9c76fbba2561aa10acb7d94d0fcf1abe
Timofey Dergachev <t@exeto.me>
2016-09-03 10:11:57 +0300

    Update tests

--- test/test.js
+++ test/test.js
@@ -5,9 +5,7 @@ import Apist from '../src';
 
 global.fetch = fetch;
 
-const user = new Apist('users');
-
-nock('http://example.com/api')
+nock('http://example.com')
   .get('/users')
   .query({ test: 'test' })
   .reply(200, [{ name: 'John' }])
@@ -27,6 +25,8 @@ nock('http://example.com/api')
   .reply(404);
 
 test('build url', t => {
+  const user = new Apist('users');
+
   t.is(user.buildUrl(), '/users');
   t.is(user.buildUrl(20), '/users/20');
   t.is(user.buildUrl(20, { name: 'John' }), '/users/20?name=John');
@@ -37,16 +37,26 @@ test('build url', t => {
   t.is(user.buildUrl(20), 'http://example.com/api/users/20');
 });
 
-test('CRUD methods', t => Promise.resolve([
-  user.fetchAll({ test: 'test' }).then(r => t.is(r[0].name, 'John')),
-  user.fetch(20, { test: 'test' }).then(r => t.is(r.name, 'John')),
-  user.create({ name: 'John' }).then(r => t.is(r.name, 'John')),
-  user.update(20, { name: 'John' }).then(r => t.is(r.name, 'John')),
-  user.delete(20).then(() => t.pass()),
-  user.fetch(404).catch(err => t.is(err.message, '404 Not Found')),
-]));
+test('CRUD methods', t => {
+  const user = new Apist('users');
+  user.host = 'http://example.com';
 
-test('combine requests', t => Promise.all([
-  user.fetch('combine'),
-  user.fetch('combine').then(() => t.pass()),
-]));
+  return Promise.all([
+    user.fetchAll({ test: 'test' }).then(r => t.is(r[0].name, 'John')),
+    user.fetch(20, { test: 'test' }).then(r => t.is(r.name, 'John')),
+    user.create({ name: 'John' }).then(r => t.is(r.name, 'John')),
+    user.update(20, { name: 'John' }).then(r => t.is(r.name, 'John')),
+    user.delete(20).then(() => t.pass()),
+    user.fetch(404).catch(err => t.is(err.message, '404 Not Found')),
+  ]);
+});
+
+test('combine requests', t => {
+  const user = new Apist('users');
+  user.host = 'http://example.com';
+
+  return Promise.all([
+    user.fetch('combine'),
+    user.fetch('combine').then(() => t.pass()),
+  ]);
+});
